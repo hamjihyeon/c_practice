48bdf5a319e933c5b75fac18884750021999d2b1
hamjihyeon <gkawlgus01@naver.com>
2022-04-07 14:55:14 +0900

    정수형증감연산자

--- c_practice/increment_integer.c
+++ c_practice/increment_integer.c
@@ -9,7 +9,7 @@ int main()
     // num1 = num1 + 1;
     // num1 += 1;
 
-    printf("증가연산자\n");
+    printf("정수형 증가연산자\n");
     printf("num1에 1더하고 출력\n");
     printf("%d\n", num1);    // 2
     printf("\n");
@@ -20,7 +20,7 @@ int main()
 
     num2--;    // 정수형 변수의 값을 1 감소시킴
 
-    printf("감소연산자\n");
+    printf("정수형 감소연산자\n");
     printf("num1에 1빼고 출력\n");
     printf("%d\n", num2);    // 1
 
