54a50b4279cf3d8c0c89fed165e2f1d2f3c2e0aa
hamjihyeon <gkawlgus01@naver.com>
2022-04-07 20:08:56 +0900

    elseif문 사용하기

---
+++ c_practice/if_elseif.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // else if문 사용하기
+    int num1 = 20;
+
+    if (num1 == 10)
+        printf("10입니다.\n");
+    else if (num1 == 20)    // else인 상태에서 조건식 지정
+        printf("20입니다.\n");
+
+    return 0;
+}
